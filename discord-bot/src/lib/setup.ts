9c74cdc9c9363626020091699adca80b13bb30da
RM3 <roger@studioavante.com>
2025-06-02 16:34:22 -0300

    added dotenv

--- discord-bot/src/lib/setup.ts
+++ discord-bot/src/lib/setup.ts
@@ -3,16 +3,11 @@ process.env.NODE_ENV ??= 'development';
 
 import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
 import '@sapphire/plugin-logger/register';
-import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
-import { join } from 'node:path';
-import { srcDir } from './constants';
+import 'dotenv/config';
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
-
-// Read env var
-setup({ path: join(srcDir, '.env') });
 
 // Enable colorette
 colorette.createColors({ useColor: true });
